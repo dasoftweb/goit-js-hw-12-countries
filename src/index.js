dd117be42ec0644ea8a64d9b2c7eae7146146c9e
Klirik <dasoftweb@gmail.com>
2021-03-08 03:39:02 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './styles.css';
+import {debounce as _debounce} from 'lodash';
+import Countries from './js/api/fetchCountries';
+
+const inputRef = document.querySelector('.search-form__input');
+inputRef.addEventListener(
+  'input',
+  _.debounce(() => {
+    searchCountry()
+  }, 500),
+);
+
+function searchCountry() {
+  if (inputRef.value) {
+    const countries = new Countries('.search-results');
+    countries.fetchCountries(inputRef.value);
+  }
+}
---
+++ src/js/api/fetchCountries.js
@@ -0,0 +1,56 @@
+import setting from './settings';
+import countryListTemplate from '../../templates/countryList.hbs';
+import countryCardTemplate from '../../templates/CountryCard.hbs';
+
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const { BASE_URL } = setting;
+
+class Countries {
+  #countries = [];
+
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+    this.#countries = [];
+  }
+
+  get countries() {
+    return this.#countries;
+  }
+
+  set countries(countriesList) {
+    if (!countriesList) {
+      return;
+    }
+
+    if (countriesList.length > 10) {
+      return error({
+        text: 'To many matches found. Please ener a more specific query!',
+      });
+    }
+
+    this.#countries = countriesList;
+    this.render();
+  }
+
+  fetchCountries(searchQuery) {
+    const url = BASE_URL + searchQuery;
+    fetch(url)
+      .then(response => {
+          return response.json();
+      })
+      .then(data => {
+        this.countries = data;
+      });
+  }
+
+  render() {
+    this.countries.length > 1
+      ? (this.element.innerHTML = countryListTemplate(this.countries))
+      : (this.element.innerHTML = countryCardTemplate(this.countries));
+  }
+}
+
+export default Countries;
